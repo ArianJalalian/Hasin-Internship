a1956a8974a871b1f205a1df56cb58a03e45d4b2
Arian Jalalian <arian.jln.arian@gmail.com>
2022-07-30 11:58:45 +0430

    initial commit

---
+++ Configuration/TodoApi/TodoApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly string version;
+        private readonly Custom custom;
+
+
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, string version, IOptions<Custom> custom)
+        {
+            _logger = logger;
+            this.version = version;  
+            this.custom = custom.Value;   
+        }
+
+        [HttpGet]
+        public /*IEnumerable<WeatherForecast> */ string Get()
+        {
+            //var rng = new Random();
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = rng.Next(-20, 55),
+            //    Summary = Summaries[rng.Next(Summaries.Length)]
+            //})
+            //.ToArray(); 
+            return custom.ConfigValue; 
+
+        }
+    }
+}
